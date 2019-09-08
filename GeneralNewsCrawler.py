5d1c1f54388e267a3bc71c1ff432b0ba38d29736
kingname <greensouth@foxmail.com>
2019-09-08 22:33:42 +0800

    增加测试样例的运行结果

--- GeneralNewsCrawler.py
+++ GeneralNewsCrawler.py
@@ -25,10 +25,12 @@ class GeneralNewsExtractor:
 
 
 if __name__ == '__main__':
-    html_list = glob.glob('**/guancha/*.html', recursive=True)
+    html_list = glob.glob('**/*.html', recursive=True)
     for html_file in html_list:
         with open(html_file, encoding='utf-8') as f:
             html = f.read()
         extractor = GeneralNewsExtractor()
         result = extractor.extract(html, noise_node_list=['//div[@class="comment-list"]'])
+        print(f'>>>>>>>>>>>>>{html_file}>>>>>>>>>>>>>')
         print(json.dumps(result, indent=2, ensure_ascii=False))
+        print('<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<')

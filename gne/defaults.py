d8fa87cbf8640d5581345e1209dee0942c60e699
kingname <greensouth@foxmail.com>
2019-11-24 12:04:18 +0800

    移除 blockquote 标签，增加 example 中的 noise_node_list

--- gne/defaults.py
+++ gne/defaults.py
@@ -46,7 +46,7 @@ TITLE_HTAG_XPATH = '//h1//text() | //h2//text() | //h3//text() | //h4//text()'
 
 TITLE_SPLIT_CHAR_PATTERN = '[-_|]'
 
-USELESS_TAG = ['style', 'script', 'link', 'video', 'iframe', 'source', 'picture', 'header']
+USELESS_TAG = ['style', 'script', 'link', 'video', 'iframe', 'source', 'picture', 'header', 'blockquote']
 
 # if one tag in the follow list does not contain any child node nor content, it could be removed
 TAGS_CAN_BE_REMOVE_IF_EMPTY = ['section', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'span']
